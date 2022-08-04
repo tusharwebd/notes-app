b42f8f261eecb854dba7fa3590723e9a5bd39799
Tushar Gupta <itgupta@amazon.com>
2022-08-04 13:28:09 +0530

    adding files to create,save and delete notes

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = ({ handleToggleDarkMode }) => {
+    return(
+        <div className="header">
+            <h1>Notes</h1>
+            <button className="toggle-button" onClick={()=> handleToggleDarkMode((previousDarkMode) => !previousDarkMode)}>Toggle Mode</button>
+        </div>
+    )
+};
+
+export default Header;
